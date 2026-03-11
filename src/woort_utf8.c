4af3ea8af75bf67cb51a40456fca227ae93d094c
mr_cino <mr_cino@cinogama.net>
2026-03-11 21:45:26 +0800

    refactor: rename wo_utf8 to woort_utf8 for naming consistency
    Rename all identifiers to follow the woort_ prefix convention used
    throughout the codebase.

--- src/woort_utf8.c
+++ src/woort_utf8.c
@@ -1,4 +1,4 @@
-#include "wo_utf8.h"
+#include "woort_utf8.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -9,7 +9,7 @@
  * @param str  指向 UTF-8 字符的指针
  * @return     该字符占用的字节数（1-4），如果无法解码则返回 1
  */
-static uint8_t wo_u8chsize(const char* str)
+static uint8_t woort_u8chsize(const char* str)
 {
     mbstate_t mb = {0};
     size_t strlength = strlen(str);
@@ -31,32 +31,32 @@ static uint8_t wo_u8chsize(const char* str)
     return 1;
 }
 
-size_t wo_u8strlen(wo_string_t u8str)
+size_t woort_u8strlen(woort_string_t u8str)
 {
     size_t strlength = 0;
     while (*u8str)
     {
         strlength++;
-        u8str += wo_u8chsize(u8str);
+        u8str += woort_u8chsize(u8str);
     }
     return strlength;
 }
 
-wo_string_t wo_u8stridxstr(wo_string_t u8str, size_t chidx)
+woort_string_t woort_u8stridxstr(woort_string_t u8str, size_t chidx)
 {
     while (chidx && *u8str)
     {
         --chidx;
-        u8str += wo_u8chsize(u8str);
+        u8str += woort_u8chsize(u8str);
     }
     return u8str;
 }
 
-wchar_t wo_u8stridx(wo_string_t u8str, size_t chidx)
+wchar_t woort_u8stridx(woort_string_t u8str, size_t chidx)
 {
     mbstate_t mb = {0};
     wchar_t wc;
-    wo_string_t target_place = wo_u8stridxstr(u8str, chidx);
+    woort_string_t target_place = woort_u8stridxstr(u8str, chidx);
     size_t parse_result = mbrtowc(&wc, target_place, strlen(target_place), &mb);
     
     if (parse_result > 0 && parse_result != (size_t)-1 && parse_result != (size_t)-2)
@@ -65,10 +65,10 @@ wchar_t wo_u8stridx(wo_string_t u8str, size_t chidx)
     return (wchar_t)(unsigned char)*target_place;
 }
 
-wo_string_t wo_u8substr(wo_string_t u8str, size_t from, size_t length, size_t* out_sub_len)
+woort_string_t woort_u8substr(woort_string_t u8str, size_t from, size_t length, size_t* out_sub_len)
 {
-    wo_string_t substr = wo_u8stridxstr(u8str, from);
-    wo_string_t end_place = wo_u8stridxstr(u8str, from + length);
+    woort_string_t substr = woort_u8stridxstr(u8str, from);
+    woort_string_t end_place = woort_u8stridxstr(u8str, from + length);
     *out_sub_len = (size_t)(end_place - substr);
     return substr;
 }
--- src/woort_utf8.h
+++ src/woort_utf8.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-typedef const char* wo_string_t;
+typedef const char* woort_string_t;
 
 /*
  * UTF-8 字符串处理工具
@@ -19,7 +19,7 @@ typedef const char* wo_string_t;
  */
 
 /* 表示无效的字符串位置 */
-#define WO_U8STR_NPOS ((size_t)(-1))
+#define WOORT_U8STR_NPOS ((size_t)(-1))
 
 /*
  * 计算 UTF-8 字符串的字符数
@@ -27,7 +27,7 @@ typedef const char* wo_string_t;
  * @param u8str  UTF-8 编码的字符串
  * @return       字符串中的字符数量（不是字节数）
  */
-size_t wo_u8strlen(wo_string_t u8str);
+size_t woort_u8strlen(woort_string_t u8str);
 
 /*
  * 获取 UTF-8 字符串中指定字符索引位置的指针
@@ -36,7 +36,7 @@ size_t wo_u8strlen(wo_string_t u8str);
  * @param chidx  字符索引（从 0 开始）
  * @return       指向该字符位置的指针
  */
-wo_string_t wo_u8stridxstr(wo_string_t u8str, size_t chidx);
+woort_string_t woort_u8stridxstr(woort_string_t u8str, size_t chidx);
 
 /*
  * 获取 UTF-8 字符串中指定字符索引位置的宽字符
@@ -45,7 +45,7 @@ wo_string_t wo_u8stridxstr(wo_string_t u8str, size_t chidx);
  * @param chidx  字符索引（从 0 开始）
  * @return       该位置的宽字符（wchar_t）
  */
-wchar_t wo_u8stridx(wo_string_t u8str, size_t chidx);
+wchar_t woort_u8stridx(woort_string_t u8str, size_t chidx);
 
 /*
  * 提取 UTF-8 字符串的子串
@@ -56,7 +56,7 @@ wchar_t wo_u8stridx(wo_string_t u8str, size_t chidx);
  * @param out_sub_len  输出参数，返回子串的字节长度
  * @return             指向子串起始位置的指针
  */
-wo_string_t wo_u8substr(wo_string_t u8str, size_t from, size_t length, size_t* out_sub_len);
+woort_string_t woort_u8substr(woort_string_t u8str, size_t from, size_t length, size_t* out_sub_len);
 
 #ifdef __cplusplus
 }
