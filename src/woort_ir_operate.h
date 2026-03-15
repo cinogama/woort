7a260b81eeaeb476e7c14bb28ffd962bc67e285f
mr_cino <mr_cino@cinogama.net>
2026-03-15 23:36:39 +0800

    feat: 扩展 IR 操作类型，添加类型转换和返回指令
    - 新增 RET/RETV 操作用于函数返回
    - 补全类型转换操作定义 (ITOS, RTOI, RTOS, STOI, STOR)
    - 修复 ITOR_init 宏的 SRC/DST 参数顺序

--- src/woort_ir_operate.h
+++ src/woort_ir_operate.h
@@ -14,6 +14,8 @@ typedef enum woort_IR_Operate_kind
     WOORT_IR_OPERATE_KIND_RTOS,
     WOORT_IR_OPERATE_KIND_STOI,
     WOORT_IR_OPERATE_KIND_STOR,
+    WOORT_IR_OPERATE_KIND_RET,
+    WOORT_IR_OPERATE_KIND_RETV,
 
 }woort_IR_Operate_kind;
 
@@ -68,9 +70,93 @@ typedef struct woort_IR_Operate_ITOR
     woort_IR_Operate_base m_op_base;
 
 }woort_IR_Operate_ITOR;
-#define woort_IR_Operate_ITOR_init(DST, SRC)        \
+#define woort_IR_Operate_ITOR_init(SRC, DST)        \
     {                                               \
         .m_op_base = _woort_IR_Operate_base_init(   \
             WOORT_IR_OPERATE_KIND_ITOR,             \
-            SRC, NULL, NULL, ITOR),                 \
+            SRC, NULL, NULL, DST),                  \
+    }
+
+typedef struct woort_IR_Operate_ITOS
+{
+    woort_IR_Operate_base m_op_base;
+
+}woort_IR_Operate_ITOS;
+#define woort_IR_Operate_ITOS_init(SRC, DST)        \
+    {                                               \
+        .m_op_base = _woort_IR_Operate_base_init(   \
+            WOORT_IR_OPERATE_KIND_ITOS,             \
+            SRC, NULL, NULL, DST),                  \
+    }
+
+typedef struct woort_IR_Operate_RTOI
+{
+    woort_IR_Operate_base m_op_base;
+
+}woort_IR_Operate_RTOI;
+#define woort_IR_Operate_RTOI_init(SRC, DST)        \
+    {                                               \
+        .m_op_base = _woort_IR_Operate_base_init(   \
+            WOORT_IR_OPERATE_KIND_RTOI,             \
+            SRC, NULL, NULL, DST),                  \
+    }
+
+typedef struct woort_IR_Operate_RTOS
+{
+    woort_IR_Operate_base m_op_base;
+
+}woort_IR_Operate_RTOS;
+#define woort_IR_Operate_RTOS_init(SRC, DST)        \
+    {                                               \
+        .m_op_base = _woort_IR_Operate_base_init(   \
+            WOORT_IR_OPERATE_KIND_RTOS,             \
+            SRC, NULL, NULL, DST),                  \
+    }
+
+typedef struct woort_IR_Operate_STOI
+{
+    woort_IR_Operate_base m_op_base;
+
+}woort_IR_Operate_STOI;
+#define woort_IR_Operate_STOI_init(SRC, DST)        \
+    {                                               \
+        .m_op_base = _woort_IR_Operate_base_init(   \
+            WOORT_IR_OPERATE_KIND_STOI,             \
+            SRC, NULL, NULL, DST),                  \
+    }
+
+typedef struct woort_IR_Operate_STOR
+{
+    woort_IR_Operate_base m_op_base;
+
+}woort_IR_Operate_STOR;
+#define woort_IR_Operate_STOR_init(SRC, DST)        \
+    {                                               \
+        .m_op_base = _woort_IR_Operate_base_init(   \
+            WOORT_IR_OPERATE_KIND_STOR,             \
+            SRC, NULL, NULL, DST),                  \
+    }
+
+typedef struct woort_IR_Operate_RET
+{
+    woort_IR_Operate_base m_op_base;
+
+}woort_IR_Operate_RET;
+#define woort_IR_Operate_RET_init()                 \
+    {                                               \
+        .m_op_base = _woort_IR_Operate_base_init(   \
+            WOORT_IR_OPERATE_KIND_RET,              \
+            NULL, NULL, NULL, NULL),                \
+    }
+
+typedef struct woort_IR_Operate_RETV
+{
+    woort_IR_Operate_base m_op_base;
+
+}woort_IR_Operate_RETV;
+#define woort_IR_Operate_RETV_init(SRC)             \
+    {                                               \
+        .m_op_base = _woort_IR_Operate_base_init(   \
+            WOORT_IR_OPERATE_KIND_RETV,             \
+            SRC, NULL, NULL, NULL),                 \
     }
