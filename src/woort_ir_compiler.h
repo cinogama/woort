2f422a5ad8f65fbcdf8ae4db3986fec40250264b
mr_cino <mr_cino@cinogama.net>
2026-03-26 22:17:40 +0800

    feat(ir): extend IRCompiler with function list and allocation tracking
    Add m_ir_functions list and allocation counters for constants and statics.
    Add woort_IRCompiler_add_constant and woort_IRCompiler_add_static functions.

--- src/woort_ir_compiler.h
+++ src/woort_ir_compiler.h
@@ -6,17 +6,26 @@ woort_ir_compiler.h
 
 #include "woort_ir_function.h"
 #include "woort_codeenv.h"
+#include "woort_ir_function.h"
+#include "woort_diagnosis.h"
+
+#include <stdbool.h>
 
 typedef struct woort_IRCompiler
 {
-    char _;
+    woort_LinkList /* woort_IRFunction */ m_ir_functions;
+    uint32_t m_constant_alloc_count;
+    uint32_t m_static_storage_alloc_count;
 
 }woort_IRCompiler;
 
 void woort_IRCompiler_init(woort_IRCompiler* ir_compiler);
 void woort_IRCompiler_deinit(woort_IRCompiler* ir_compiler);
 
-woort_IRFunction* woort_IRCompiler_add_function(
-    woort_IRCompiler* c, uint32_t param_count);
+WOORT_NODISCARD bool woort_IRCompiler_add_function(
+    woort_IRCompiler* c, uint32_t param_count, woort_IRFunction** out_f);
+
+woort_IRConstantIndex woort_IRCompiler_add_constant(woort_IRCompiler* c);
+woort_IRStaticIndex woort_IRCompiler_add_static(woort_IRCompiler* c);
 
 bool woort_IRCompiler_finish(woort_IRCompiler* c, woort_CodeEnv** out_cenv);
