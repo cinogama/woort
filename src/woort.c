577b50a1ee3501a960c3a271d5b17c45c3db2ca7
mr_cino <mr_cino@cinogama.net>
2026-04-04 22:44:52 +0800

    feat(codeenv): implement constant setters for CodeEnv with GC write barriers

--- src/woort.c
+++ src/woort.c
@@ -14,6 +14,7 @@
 #include "woort_gc_closure.h"
 
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 
 void woort_init(void)
@@ -51,3 +52,137 @@ void woort_IRCompiler_close(woort_IRCompiler* c)
 }
 
 /* Runtime API */
+
+void woort_CodeEnv_set_const_int(
+    woort_CodeEnv* code_env,
+    woort_IRConstantIndex cidx,
+    woort_Int val)
+{
+    assert(code_env != NULL);
+    assert((size_t)cidx < code_env->m_data_count);
+
+    woort_Value v;
+    v.m_integer = val;
+
+    woort_GC_mixed_write_barrier_value(&code_env->m_data_begin[cidx], v);
+}
+
+void woort_CodeEnv_set_const_real(
+    woort_CodeEnv* code_env,
+    woort_IRConstantIndex cidx,
+    woort_Real val)
+{
+    assert(code_env != NULL);
+    assert((size_t)cidx < code_env->m_data_count);
+
+    woort_Value v;
+    v.m_real = val;
+
+    woort_GC_mixed_write_barrier_value(&code_env->m_data_begin[cidx], v);
+}
+
+void woort_CodeEnv_set_const_string(
+    woort_CodeEnv* code_env,
+    woort_IRConstantIndex cidx,
+    woort_U8CString val)
+{
+    assert(code_env != NULL);
+    assert((size_t)cidx < code_env->m_data_count);
+    assert(val != NULL);
+
+    size_t len = strlen(val);
+    const woort_GCString* str = woort_GCString_make_string(val, len);
+    assert(str != NULL);
+
+    woort_Value v;
+    v.m_string = str;
+
+    woort_GC_mixed_write_barrier_value(&code_env->m_data_begin[cidx], v);
+}
+
+void woort_CodeEnv_set_const_script_function(
+    woort_CodeEnv* code_env,
+    woort_IRConstantIndex cidx,
+    const woort_Bytecode* val)
+{
+    assert(code_env != NULL);
+    assert((size_t)cidx < code_env->m_data_count);
+
+    woort_Value v;
+    v.m_script_function = val;
+
+    woort_GC_mixed_write_barrier_value(&code_env->m_data_begin[cidx], v);
+}
+
+void woort_CodeEnv_set_const_extern_function(
+    woort_CodeEnv* code_env,
+    woort_IRConstantIndex cidx,
+    const woort_Bytecode* val)
+{
+    assert(code_env != NULL);
+    assert((size_t)cidx < code_env->m_data_count);
+
+    woort_Value v;
+    v.m_script_function = val;
+
+    woort_GC_mixed_write_barrier_value(&code_env->m_data_begin[cidx], v);
+}
+
+void woort_CodeEnv_set_const_script_closure(
+    woort_CodeEnv* code_env,
+    woort_IRConstantIndex cidx,
+    const woort_Bytecode* val)
+{
+    assert(code_env != NULL);
+    assert((size_t)cidx < code_env->m_data_count);
+
+    woort_GCClosure* closure = woort_GCClosure_new_script_func(val);
+    assert(closure != NULL);
+
+    woort_Value v;
+    v.m_closure = closure;
+
+    woort_GC_mixed_write_barrier_value(&code_env->m_data_begin[cidx], v);
+}
+
+void woort_CodeEnv_set_const_extern_closure(
+    woort_CodeEnv* code_env,
+    woort_IRConstantIndex cidx,
+    const woort_Bytecode* val)
+{
+    assert(code_env != NULL);
+    assert((size_t)cidx < code_env->m_data_count);
+
+    woort_NativeFunction native_func = (woort_NativeFunction)val;
+    woort_GCClosure* closure = woort_GCClosure_new_native_func(native_func);
+    assert(closure != NULL);
+
+    woort_Value v;
+    v.m_closure = closure;
+
+    woort_GC_mixed_write_barrier_value(&code_env->m_data_begin[cidx], v);
+}
+
+void woort_CodeEnv_set_const_box_int(
+    woort_CodeEnv* code_env,
+    woort_IRConstantIndex cidx,
+    woort_Int val)
+{
+    assert(code_env != NULL);
+    assert((size_t)cidx < code_env->m_data_count);
+
+    woort_DynBox boxed = woort_DynBox_box_int(val);
+    woort_GC_mixed_write_barrier_dynbox(&code_env->m_data_begin[cidx].m_dynamic, boxed);
+}
+
+void woort_CodeEnv_set_const_box_real(
+    woort_CodeEnv* code_env,
+    woort_IRConstantIndex cidx,
+    woort_Real val)
+{
+    assert(code_env != NULL);
+    assert((size_t)cidx < code_env->m_data_count);
+
+    woort_DynBox boxed = woort_DynBox_box_real(val);
+    woort_GC_mixed_write_barrier_dynbox(&code_env->m_data_begin[cidx].m_dynamic, boxed);
+}
