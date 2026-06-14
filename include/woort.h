73c1e68b8e48c0a527843ec6c5a02789ce60dd4d
mr_cino <mr_cino@cinogama.net>
2026-06-14 23:28:50 +0800

    * Releas 1.0.4.2

--- include/woort.h
+++ include/woort.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /** @brief Woort version encoded as (major, minor, patch, tweak). */
-#define WOORT_VERSION WOORT_VERSION_WRAP(1, 0, 4, 1)
+#define WOORT_VERSION WOORT_VERSION_WRAP(1, 0, 4, 2)
 
 #ifndef WOORT_MSVC_RC_INCLUDE
 
