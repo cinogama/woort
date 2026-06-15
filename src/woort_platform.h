e1ae0a45a48a9fafce29820d7c4051eccd375c20
mr_cino <mr_cino@cinogama.net>
2026-06-16 01:02:04 +0800

    feat: add WOORT_SPECIFY_PLATFORM_TYPE option for manual platform specification

--- src/woort_platform.h
+++ src/woort_platform.h
@@ -22,19 +22,29 @@ woort_platform.h - Platform detection macros
 
 /* Architecture detection */
 #if defined(_M_IX86) || defined(__i386__)
-#   define WOORT_PLATFORM_32
+#   ifndef WOORT_PLATFORM_32
+#       define WOORT_PLATFORM_32
+#   endif
 #   define WOORT_PLATFORM_X86
 #elif defined(__x86_64__) || defined(_M_X64)
-#   define WOORT_PLATFORM_64
+#   ifndef WOORT_PLATFORM_64
+#       define WOORT_PLATFORM_64
+#   endif
 #   define WOORT_PLATFORM_X64
 #elif defined(_M_ARM) || defined(__arm__)
-#   define WOORT_PLATFORM_32
+#   ifndef WOORT_PLATFORM_32
+#       define WOORT_PLATFORM_32
+#   endif
 #   define WOORT_PLATFORM_ARM
 #elif defined(__aarch64__) || defined(_M_ARM64)
-#   define WOORT_PLATFORM_64
+#   ifndef WOORT_PLATFORM_64
+#       define WOORT_PLATFORM_64
+#   endif
 #   define WOORT_PLATFORM_ARM64
 #else
-#   if !defined(WOORT_PLATFORM_32) && !defined(WOORT_PLATFORM_64)
+#   if defined(WOORT_PLATFORM_32) && defined(WOORT_PLATFORM_64)
+#       error "WOORT_PLATFORM_32 and WOORT_PLATFORM_64 both defined, unexpected."
+#   elif !defined(WOORT_PLATFORM_32) && !defined(WOORT_PLATFORM_64)
 #       error "Unknown platform, you must specify platform manually."
 #   endif
 #endif
