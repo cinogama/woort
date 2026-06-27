8fa2d384d3e34041b45bcec5e72b7a0778826cd5
mr_cino <mr_cino@cinogama.net>
2026-06-27 12:37:28 +0800

    refactor: extract GCUnit and Value type definitions into separate _types.h headers

--- src/woort_gc_units.h
+++ src/woort_gc_units.h
@@ -4,30 +4,13 @@
 woort_gc_units.h
 */
 
-#include "woort.h"
+#include "woort_gc_units_types.h"
 
 #include "woomem.h"
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-
-typedef struct woort_GCUnit woort_GCUnit;
-
-typedef void(*woort_GCUnitProxy_MarkCallback)(woort_GCUnit*);
-typedef void(*woort_GCUnitProxy_DestructCallback)(woort_GCUnit*);
-
-typedef struct woort_GCUnitProxy
-{
-    /* OPTIONAL */ woort_GCUnitProxy_MarkCallback m_marker;
-    /* OPTIONAL */ woort_GCUnitProxy_DestructCallback m_destructor;
-
-}woort_GCUnitProxy;
-
-struct woort_GCUnit
-{
-    const woort_GCUnitProxy* m_proxy;
-};
 
 #define WOORT_GCUNIT_ALLOC_ATTRIB_O 0
 #define WOORT_GCUNIT_ALLOC_ATTRIB_A WOOMEM_ATTRIB_AUTO_MARK
--- src/woort_value.h
+++ src/woort_value.h
@@ -4,31 +4,13 @@
 woort_value.h
 */
 
-#include "woort.h"
+#include "woort_value_types.h"
 
 #include "woort_gc_units.h"
 
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-
-typedef uint64_t woort_BoxedValue;
-typedef uint64_t woort_BoxedFloat63;
-typedef uint64_t woort_BoxedInt62;
-typedef uint64_t woort_BoxedBool;
-
-typedef struct woort_GCString woort_GCString;
-typedef struct woort_GCMap woort_GCMap;
-typedef struct woort_GCVec woort_GCVec;
-typedef struct woort_GCStruct woort_GCStruct;
-typedef struct woort_GCClosure woort_GCClosure;
-typedef struct woort_GCHandle woort_GCHandle;
-
-typedef union woort_DynBox
-{
-    woort_BoxedValue m_boxed;
-
-}woort_DynBox;
 
 _Static_assert(sizeof(woort_DynBox) == sizeof(woort_BoxedValue),
     "woort_DynBox must have same size as woort_BoxedValue");
@@ -50,50 +32,6 @@ static inline struct woort_BoxedExValue* _woort_boxed_to_exvalue(
 {
     return (struct woort_BoxedExValue*)(uintptr_t)val;
 }
-
-typedef enum woort_CallWay
-{
-    // 一个脚本中的函数调用了另一个（本地的）脚本函数
-    WOORT_CALL_WAY_NEAR,
-
-    // 调用了另一个代码环境下的脚本函数，返回时需要额外检查
-    WOORT_CALL_WAY_FAR,
-
-    // 此调用是由 native 层发起的，返回时需要中断解释器执行
-    WOORT_CALL_WAY_FROM_NATIVE,
-
-} woort_CallWay;
-
-typedef struct woort_RetBP
-{
-    woort_CallWay   m_way;
-    uint32_t        m_bp_offset;
-
-} woort_RetBP;
-
-union woort_Value
-{
-    woort_Value*            m_pvalue;
-    woort_GCUnit*           m_gcinstance;
-    woort_Int               m_integer;
-    woort_Real              m_real;
-    const woort_GCString*   m_string;
-    woort_GCVec*            m_vec;
-    woort_GCMap*            m_map;
-    woort_GCStruct*         m_struct;
-    const woort_GCClosure*  m_closure;
-    const woort_GCHandle*   m_gchandle;
-
-    const woort_Bytecode*   m_script_function;
-    woort_NativeFunction    m_native_function;
-    woort_JitFunction       m_jit_function;
-
-    woort_DynBox            m_dynamic;
-
-    woort_RetBP             m_ret_bp;
-    const void* m_ret_addr;
-
-};
 
 woort_DynBox woort_DynBox_box_real(woort_Real val);
 woort_DynBox woort_DynBox_box_int(woort_Int val);
@@ -149,18 +87,6 @@ WOORT_NODISCARD bool _woort_unbox_bool(uint64_t val);
 
 // 扩展装箱类型的 proxy
 extern const woort_GCUnitProxy WOORT_EX_BOX_PROXY;
-
-// 扩展装箱类型：用于存储超出内联范围的整数或浮点数
-typedef struct woort_BoxedExValue
-{
-    woort_GCUnit m_unit;
-    bool m_is_int;
-    union
-    {
-        woort_Real m_real;
-        woort_Int m_int;
-    };
-} woort_BoxedExValue;
 
 ////////////////////////////////////////////////////////////////////////
 // 类型特化的比较函数：避免装箱分配
