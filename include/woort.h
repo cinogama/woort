7c96db1cb555a1e3c988c65891194afe6d52cec8
mr_cino <mr_cino@cinogama.net>
2026-07-14 00:28:23 +0800

    chore: bump woort version to 1.0.6.5

--- include/woort.h
+++ include/woort.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /** @brief Woort version encoded as (major, minor, patch, tweak). */
-#define WOORT_VERSION WOORT_VERSION_WRAP(1, 0, 6, 4)
+#define WOORT_VERSION WOORT_VERSION_WRAP(1, 0, 6, 5)
 
 #ifndef WOORT_MSVC_RC_INCLUDE
 
