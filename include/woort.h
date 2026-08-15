00a5c203a55825f4462513cf931d41ea95efa60b
mr_cino <mr_cino@cinogama.net>
2026-08-15 14:17:32 +0800

    * Release 1.0.6.15

--- include/woort.h
+++ include/woort.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /** @brief Woort version encoded as (major, minor, patch, tweak). */
-#define WOORT_VERSION WOORT_VERSION_WRAP(1, 0, 6, 14)
+#define WOORT_VERSION WOORT_VERSION_WRAP(1, 0, 6, 15)
 
 #ifndef WOORT_MSVC_RC_INCLUDE
 
